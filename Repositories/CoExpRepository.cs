85889cc1fe3cf67278e2b0772d883d9098472298
Sonia Garcia <43370296+SoniaRuiz@users.noreply.github.com>
2019-04-03 11:38:57 +0100

    API test

--- Repositories/CoExpRepository.cs
+++ Repositories/CoExpRepository.cs
@@ -28,7 +28,7 @@ namespace CoExp_Web.Repositories
         public CoExpRepository()
         {
             _adapter = new HttpAdapter();
-            _coexpURL = "https://snca.atica.um.es/api/";
+            _coexpURL = "https://snca.atica.um.es/api_test/";
         }
         /// <summary>
         /// Method to obtain data from 'getNetworkCategories' API method
